03f944ffa7e2d218c5769f5ce4a49392ce77ae27
ridwanreformasi <ridwanreformasi86@gmail.com>
2024-10-07 12:29:19 +0800

    Add files via upload

--- App.java
+++ App.java
@@ -1,28 +1,32 @@
-// Mahasiswa class sebagai
-class Mahasiswa {
-    // Ciri-ciri atau atribut objek
-    String Nama;
-    String NIM;
-    String Jurusan;
-    String IPK;
-    String Umur;
+class Kalkulator{
+    // atribut
+    double Perkalian;
+    double Penjumlahan;
+    double Pengurangan;
+    double Pembagian;
+
+    // constructor dengan parameter
+    Kalkulator (double Input1, double Input2, double Input3, double Input4){
+        Perkalian = Input1 * Input2 * Input3 * Input4;
+        Penjumlahan = Input1 + Input2 + Input3 + Input4;
+        Pengurangan = Input1 - Input2 - Input3 - Input4;
+        Pembagian = Input1 / Input2 / Input3 / Input4;
+        
+        // cetak
+        System.out.println("Hasil Perkalian: " + Perkalian);
+        System.out.println("Hasil Penjumlahan: " + Penjumlahan);
+        System.out.println("Hasil Pengurangan: " + Pengurangan);
+        System.out.println("Hasil Pembagian: " + Pembagian);
+    }
 }
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // instalasi objek
-        Mahasiswa mhs_ = new Mahasiswa();
-        mhs_.Nama = "ido";
-        mhs_.NIM = "23241012"; 
-        mhs_.Jurusan = "PTI";
-        mhs_.IPK = "3.57";
-        mhs_.Umur = "20";
 
-        // cetak objek
-        System.out.println("Nama Mahasiswa : " + mhs_.Nama);
-        System.out.println("NIM : " + mhs_.NIM);
-        System.out.println("Jurusan : " + mhs_.Jurusan);
-        System.out.println("IPK : " + mhs_.IPK);
-        System.out.println("Umur : " + mhs_.Umur);
+        // instansi object class kalkulator
+        Kalkulator Kalkulator1 = new Kalkulator(5, 2, 4, 10);
+
+        Kalkulator kalkulator2 = new Kalkulator(10, 5, 8, 4);
     }
 }
